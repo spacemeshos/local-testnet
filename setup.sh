3c9f681b7e3a29ce492ac8c79521dd1cb4a13d5d
anton <anton.lerner@gmail.com>
2019-07-09 19:26:02 +0300

    switched from go get to git pull, added requirements file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,24 @@
+#! /bin/sh -x
+
+GITHUB=https://github.com
+SPACEMESHOS=$GITHUB/spacemeshos
+GO_GITHUB=~/go/src/github.com/spacemeshos
+
+
+git clone $SPACEMESHOS/go-spacemesh.git $GO_GITHUB/go-spacemesh
+git clone $SPACEMESHOS/poet.git $GO_GITHUB/poet
+git clone $SPACEMESHOS/CLIWallet.git $GO_GITHUB/CLIWallet
+
+echo "building node"
+cd $GO_GITHUB/go-spacemesh
+make dockerbuild-go || echo "building spacemsh node failed"
+
+echo "building poet"
+cd $GO_GITHUB/poet
+make dockerbuild-go || echo "building poet failed"
+
+cd $GO_GITHUB/CLIWallet
+go get && go build || echo "building wallet failed"
 #! /bin/sh
 
 SPACEMESHOS=spacemeshos
