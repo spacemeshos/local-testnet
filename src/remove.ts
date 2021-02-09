7796bd71844ffef8db6a5883e7bb71c7c082460b
Narayan Prusty <arsenal.narayan@gmail.com>
2021-02-09 13:49:26 +0530

    remove elk script

--- src/remove.ts
+++ src/remove.ts
@@ -1,41 +1,43 @@
 import { getDockerClient } from './utils';
 import boxen from 'boxen';
 import chalk from 'chalk';
+import { down } from 'docker-compose';
+import path from 'path';
 
 export default async () => {
-    try {
-        console.log(
-            boxen('Deleting Network', {
-                padding: 1,
-                borderColor: 'blue',
-                borderStyle: 'classic'
-            })
-        );
-        const docker = await getDockerClient();
-        const containers = await docker.container.list();
+  try {
+    console.log(
+      boxen('Deleting Network', {
+        padding: 1,
+        borderColor: 'blue',
+        borderStyle: 'classic'
+      })
+    );
 
-        const promises = [];
+    await down({ cwd: `${path.join(__dirname)}/elk`, log: true });
 
-        containers.forEach(container => {
-            const data = JSON.parse(JSON.stringify(container.data));
-            if (
-                data.Names[0].includes('miner') ||
-                data.Names[0].includes('poet')
-            ) {
-                promises.push(container.delete({ force: true }));
-            }
-        });
+    const docker = await getDockerClient();
+    const containers = await docker.container.list();
 
-        await Promise.all(promises);
+    const promises = [];
 
-        console.log(
-            boxen('Network Deleted', {
-                padding: 1,
-                borderColor: 'blue',
-                borderStyle: 'classic'
-            })
-        );
-    } catch (e) {
-        console.log(chalk.bold.red('Error: ' + e.message));
-    }
+    containers.forEach(container => {
+      const data = JSON.parse(JSON.stringify(container.data));
+      if (data.Names[0].includes('miner') || data.Names[0].includes('poet')) {
+        promises.push(container.delete({ force: true }));
+      }
+    });
+
+    await Promise.all(promises);
+
+    console.log(
+      boxen('Network Deleted', {
+        padding: 1,
+        borderColor: 'blue',
+        borderStyle: 'classic'
+      })
+    );
+  } catch (e) {
+    console.log(chalk.bold.red('Error: ' + e.message));
+  }
 };
